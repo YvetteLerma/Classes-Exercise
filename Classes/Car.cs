314e550a73a29303d770f15d7fe4a6157b11c48e
Yvette Lerma <lerma.yvette@hotmail.com>
2022-10-27 14:00:50 -0600

    code added

---
+++ Classes/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Classes
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+        //Constructors
+        //- a special member method that has the same name as its class
+        public Car(string make, string model, int year)
+          
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+
+        //fields
+        //properties
+        //methods
+
+        //Create a Make propert of type string that is public
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        
+    }
+}
+
